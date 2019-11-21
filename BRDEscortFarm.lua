e479ff1d3a0a1aa4f37611a0fd285fe65f772d25
Andrew Potter <apottere@gmail.com>
2019-11-20 19:29:42 -0500

    Track health and mana

--- BRDEscortFarm.lua
+++ BRDEscortFarm.lua
@@ -26,35 +26,59 @@ local function ui(frame)
     return _G['BRDEscortFarm_Frame_' .. frame]
 end
 
-local function unitPowerPercentage(unit)
-    local currentPower = UnitPower(unit)
-    local maxPower = UnitPowerMax(unit)
-    if maxPower == 0 then
+local function percentage(current, max)
+    if current == nil or max == nil or max == 0 then
         return nil
     end
 
-    return math.floor((currentPower / maxPower) * 100)
+    return math.floor((current / max) * 100)
 end
 
-local function displayUnitLine()
+local function unitPowerPercentage(unit)
+    local currentPower = UnitPower(unit)
+    local maxPower = UnitPowerMax(unit)
+
+    return percentage(currentPower, maxPower)
 end
 
-function BRDEscortFarm:RunTick()
-    local memberCount = GetNumGroupMembers(LE_PARTY_CATEGORY_INSTANCE);
-    print(tostring(UnitHealthMax(9020)))
+local function updateUnitLine(id)
+    local name = UnitName(id)
+    local power = unitPowerPercentage(id)
+
+    if name ~= nil then
+        ui(id .. "Name"):SetText(name)
+    else
+        ui(id .. "Name"):SetText("???")
+    end
+
+    if power ~= nil then
+        ui(id .. "Power"):SetText(tostring(power))
+    else
+        ui(id .. "Power"):SetText("???")
+    end
 end
 
-function BRDEscortFarm:OnEvent(event, target)
-    local guid = UnitGUID(target .. "target")
+local function targetIsGorshak()
+    local guid = UnitGUID("playertarget")
     if guid == nil then
         return;
     end
 
-    print(guid)
-
     local id = guid:sub(-15, -12)
-    print(tostring(id))
-    if id == "9020" then
-        print(UnitHealthMax(target .. "target"))
+    return id == "9020"
+end
+
+function BRDEscortFarm:RunTick()
+    local gorshakHealth = ui("gorshakHealth")
+    if targetIsGorshak() then
+        gorshakHealth:SetText(percentage(UnitHealth("target"), UnitHealthMax("target")))
+    else
+        gorshakHealth:SetText("???")
     end
+
+    updateUnitLine("player")
+    updateUnitLine("party1")
+    updateUnitLine("party2")
+    updateUnitLine("party3")
+    updateUnitLine("party4")
 end
